d39bda5df42158118a20f92d4836c63de33bf0d3
Aditya Kusupati <adityakusupati2@gmail.com>
2023-05-03 15:46:32 -0700

    MRL + CLIP

--- docs/script_examples/stability_example.sh
+++ docs/script_examples/stability_example.sh
@@ -29,10 +29,10 @@ export FI_PROVIDER=efa
 export FI_EFA_TX_MIN_CREDITS=64
 export NCCL_TREE_THRESHOLD=0
 
-cd /admin/home-mitchellw/open_clip/src
-export PYTHONPATH="$PYTHONPATH:/admin/home-mitchellw/open_clip/src"
+cd /fsx/home-kusupati/open_clip/src
+export PYTHONPATH="$PYTHONPATH:/fsx/home-kusupati/open_clip/src"
 
-EXP_NAME="test-B-32-laion5b-lr1e-3-bs90k"
+EXP_NAME="test-B-32-laion5b-lr1e-3-bs90k-mrl"
 
 srun --comment laion --cpu_bind=v --accel-bind=gn python -m training.main \
     --save-frequency 1 \
@@ -44,6 +44,7 @@ srun --comment laion --cpu_bind=v --accel-bind=gn python -m training.main \
     --batch-size=375 \
     --epochs=97 \
     --lr 1e-3 \
+    --mrl-dims=3 \
     --workers=8 \
     --report-to wandb \
     --name ${EXP_NAME} \
@@ -55,6 +56,6 @@ srun --comment laion --cpu_bind=v --accel-bind=gn python -m training.main \
     --gather-with-grad \
     --grad-checkpointing \
     --precision amp_bfloat16 \
-    --wandb-project-name open_clip6 \
+    --wandb-project-name open_clip_mrl \
     --resume "latest" \
-    --remote-sync s3://s-laion/mitchellw/logs
+    --remote-sync s3://s-laion/home-kusupati/logs
